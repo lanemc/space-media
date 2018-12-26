e8258cc4fba1f34d38322392f6ec64632b50fa93
unknown <lanemcunningham@gmail.com>
2018-12-25 20:10:40 -0600

    .gitignore, components dir, nav header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from './components/Nav';
 
 class App extends React.Component {
   state = {
@@ -7,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-
+        <Nav />
       </div>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">Space Media</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Nav;
